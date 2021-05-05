ca92f1dc54ee9233b0d3c2bcb712c1662cc515f6
markus-sch <markusschlussmeier@gmail.com>
2021-05-05 21:35:04 +0200

    Erstes kleines Programm

---
+++ src/DataTypes.java
@@ -0,0 +1,14 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b1 = 21;
+        short s1 = 255;
+        int i1 = 250;
+        long l1 = 2555;
+        double d1 = 255.25;
+        float f1 = 2520.2f;
+        boolean b2 = true;
+        char c1 = 'A';
+
+        System.out.println("Byte is: "+ b1);
+    }
+}
